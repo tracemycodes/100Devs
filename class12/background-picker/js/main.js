d7621daf9c544f03a0ae60ebab88cbc2fe37dfc1
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-01 04:04:27 -0700

    tip claculator html

--- class12/background-picker/js/main.js
+++ class12/background-picker/js/main.js
@@ -1,19 +1,28 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+const UIpantImg = document.querySelectorAll('.pant-img');
+const UIshirtImg = document.querySelectorAll('.shirt-img');
 
+UIpantImg.forEach(item => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault()
+    const UIpantCard = document.querySelector('.pants-card'),
+          UIpantSec = document.querySelector('.pants-section');
 
-function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
-}
+    let scrValue = e.target.attributes.src.value;
+    UIpantCard.style.backgroundImage  = `url(${scrValue})`;
+    UIpantCard.classList.toggle('transistion-left');
+    UIpantSec.style.backgroundImage  = `url(${scrValue})`;
+  })
+})
 
-function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
-}
+UIshirtImg.forEach(item => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault()
+    const UIshirtCard = document.querySelector('.shirts-card'),
+          UIshirtSec = document.querySelector('.shirts-section');
 
-function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
-}
+    let scrValue = e.target.attributes.src.value;
+    UIshirtCard.style.backgroundImage  = `url(${scrValue})`;
+    UIshirtCard.classList.toggle('transistion-left');
+    UIshirtSec.style.backgroundImage  = `url(${scrValue})`;
+  })
+})
