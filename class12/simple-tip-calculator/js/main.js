df2c2a0c862fc1023ed4ac7edcc0a0cd47eccf3b
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-04 20:12:59 -0700

    UI calculate variables declared

--- class12/simple-tip-calculator/js/main.js
+++ class12/simple-tip-calculator/js/main.js
@@ -1,26 +1,6 @@
-let total = 0
+const billInput = document.querySelector('#tip-price').value
+const personInput = document.querySelector('#tip-person').value
+const percentageInput = document.querySelectorAll('.tip-percentage')
 
-document.querySelector('#pumpkin').addEventListener('mouseenter', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
 
-function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
-}
-
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
-}
-
-function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
-}
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
-}
+// console.log(billInput, personInput, percentageInput);
