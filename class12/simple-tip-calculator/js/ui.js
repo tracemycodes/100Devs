c0f453c9f2836c8c3ae10b0bad570f85e38f8dd0
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-06 03:55:28 -0700

    refactored code base

--- class12/simple-tip-calculator/js/ui.js
+++ class12/simple-tip-calculator/js/ui.js
@@ -1,5 +1,24 @@
 class Ui {
   constructor() {
-
+    this.tipPercentage = document.getElementById('tip-percentage');
+    this.totalPercentage = document.getElementById('total-percentage');
+  }
+  loading() {
+    document.querySelector('.calculated-tip').style.display = 'block'
+    document.querySelector('.loading-spinner-blocks').style.display = 'none'
+  }
+  showCalculatedTip(solution) {
+    this.tipPercentage.textContent = `$${solution.tipPerPerson}`
+    this.totalPercentage.textContent = `$${solution.amountPerPerson}`
+    document.querySelector('.calculated-tip').style.display = 'none'
+    document.querySelector('.loading-spinner-blocks').style.display = 'inline-block'
+    setTimeout(this.loading, 2000)
+  }
+  resetUi(billInput, customInput, personInput) {
+    billInput.value = ''
+    customInput.value = ''
+    personInput.value = ''
+    this.tipPercentage.textContent = `$0.00`
+    this.totalPercentage.textContent = `$0.00`
   }
 }
