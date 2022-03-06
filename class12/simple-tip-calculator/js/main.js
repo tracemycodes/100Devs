d95c559f97133484bd145b7065d1105c14146c01
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-06 03:55:51 -0700

    refactored code base

--- class12/simple-tip-calculator/js/main.js
+++ class12/simple-tip-calculator/js/main.js
@@ -1,43 +1,52 @@
-const billInput = document.querySelector('#tip-price');
-const personInput = document.querySelector('#tip-person');
-const percentageInput = document.querySelector('.button-field');
-const customInput = document.querySelector('#btn-num');
+const billInput = document.querySelector("#tip-price"),
+      personInput = document.querySelector("#tip-person"),
+      percentageInput = document.querySelector(".button-field"),
+      customInput = document.querySelector("#btn-num"),
+      restBtn = document.getElementById('reset-btn');
 
 const calculate = new Calculate();
 const ui = new Ui();
 
-
-
 percentageInput.addEventListener("click", (item) => {
   if (item.target.classList.contains("tip-percentage")) {
     percentageValue = item.target.value;
-    calculate.getPercentageValue(percentageValue, billInput.value, personInput.value);
-    
-    if (calculate.people != "" && calculate.bill != "") {
-      let answer = calculate.calculateTip()
-      // console.log(answer);
-    }
+    calculate.getPercentageValue(
+      percentageValue,
+      billInput.value,
+      personInput.value
+    );
 
-  } 
+    if (calculate.people != "" && calculate.bill != "") {
+      let solution = calculate.calculateTip();
+      ui.showCalculatedTip(solution);
+    }
+  }
 });
 
-personInput.addEventListener('input', () => {
-  console.log(typeof personInput.value);
-
+personInput.addEventListener("input", () => {
   if (Number(personInput.value) === 0) {
-    
   } else {
-    let answer = calculate.calculateTip()
+    let solution = calculate.calculateTip();
+    ui.showCalculatedTip(solution);
   }
+});
 
-})
+customInput.addEventListener("input", () => {
+  console.log(customInput.value);
+  if (customInput.value === "") {
+    return;
+  } else {
+    calculate.getPercentageValue(
+      customInput.value,
+      billInput.value,
+      personInput.value
+    );
 
-customInput.addEventListener('input', () => {
-  calculate.getPercentageValue(customInput.value, billInput.value, personInput.value);
- 
-  if (calculate.people != "" && calculate.bill != "") {
-      let answer = calculate.calculateTip()
+    let solution = calculate.calculateTip();
+    if (calculate.people != "" && calculate.bill != "") {
+      ui.showCalculatedTip(solution);
+    }
   }
-})
+});
 
-
+restBtn.onclick = () => ui.resetUi(billInput, customInput, personInput);
