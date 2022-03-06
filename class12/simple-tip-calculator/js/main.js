f4c503d042b5c611620bd4f5d0442cf5907f386d
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-05 17:50:01 -0700

    refactored object

--- class12/simple-tip-calculator/js/main.js
+++ class12/simple-tip-calculator/js/main.js
@@ -1,22 +1,31 @@
-const billInput = document.querySelector('#tip-price')
-const personInput = document.querySelector('#tip-person')
+const billInput = document.querySelector('#tip-price');
+const personInput = document.querySelector('#tip-person');
 const percentageInput = document.querySelector('.button-field');
-const customInput = document.querySelector('#btn-num')
+const customInput = document.querySelector('#btn-num');
 
-const calculate = new Calculate(billInput, customInput.value, personInput);
+const calculate = new Calculate();
 
-// const getPercentageValue = () => {
-//   let percentageValue;
-//   percentageInput.addEventListener('click', item => {
-//   if (item.target.classList.contains('tip-percentage')) {
-//     percentageValue = item.target.textContent   
-//   } else if (item.target.className = 'custom') {
-//     percentageValue = item.target.value
-//   }
-//   return percentageValue;
-// })
+
+
+percentageInput.addEventListener("click", (item) => {
+  if (item.target.classList.contains("tip-percentage")) {
+    percentageValue = item.target.value;
+    calculate.getPercentageValue(percentageValue, billInput.value, personInput.value);
+    
+    if (calculate.people != "" && calculate.bill != "") {
+      let answer = calculate.calculateTip()
+    }
+
+    // console.log(calculate.bill);
+  } 
+});
+
+
+
+// personInput.addEventListener('input', rechout)
+
+// function rechout () {
+//   console.log("hello");
 // }
 
-calculate.getPercentageValue(percentageInput)
-
-console.log(calculate);
+// console.log(calculate);
