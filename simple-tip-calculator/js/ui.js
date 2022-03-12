3e47e03e112b44e141e3356c3936d7509556559a
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-12 09:32:47 -0700

    refactored code base

--- simple-tip-calculator/js/ui.js
+++ simple-tip-calculator/js/ui.js
@@ -1,24 +1,31 @@
 class Ui {
   constructor() {
-    this.tipPercentage = document.getElementById('tip-percentage');
-    this.totalPercentage = document.getElementById('total-percentage');
+    this.tipPercentage = document.getElementById("tip-percentage");
+    this.totalPercentage = document.getElementById("total-percentage");
   }
   loading() {
-    document.querySelector('.calculated-tip').style.display = 'block'
-    document.querySelector('.loading-spinner-blocks').style.display = 'none'
+    document.querySelector(".calculated-tip").style.display = "block";
+    document.querySelector(".loading-spinner-blocks").style.display = "none";
   }
   showCalculatedTip(solution) {
-    this.tipPercentage.textContent = `$${solution.tipPerPerson.toFixed(2)}`
-    this.totalPercentage.textContent = `$${solution.amountPerPerson.toFixed(2)}`
-    document.querySelector('.calculated-tip').style.display = 'none'
-    document.querySelector('.loading-spinner-blocks').style.display = 'inline-block'
-    setTimeout(this.loading, 2000)
+    this.tipPercentage.textContent = `$${solution.tipPerPerson.toFixed(2)}`;
+    this.totalPercentage.textContent = `$${solution.amountPerPerson.toFixed(
+      2
+    )}`;
+    document.querySelector(".calculated-tip").style.display = "none";
+    document.querySelector(".loading-spinner-blocks").style.display =
+      "inline-block";
+    setTimeout(this.loading, 2000);
   }
-  resetUi(billInput, customInput, personInput) {
-    billInput.value = ''
-    customInput.value = ''
-    personInput.value = ''
-    this.tipPercentage.textContent = `$0.00`
-    this.totalPercentage.textContent = `$0.00`
+  resetUi(billInput, customInput, personInput, tipBtn) {
+    billInput.value = "";
+    customInput.value = "";
+    personInput.value = "";
+    this.tipPercentage.textContent = `$0.00`;
+    this.totalPercentage.textContent = `$0.00`;
+
+    tipBtn.forEach((btn) => {
+      btn.classList.remove('active-btn')
+    });
   }
 }
